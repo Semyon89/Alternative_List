d46002bdd2fb1565dfbc82d46f254e95251f333b
Semyon89 <getShelukhin@gmail.com>
2019-01-06 19:59:38 +0300

    (Update2)

--- Alternative_List/Alternative_List/New_List.h
+++ Alternative_List/Alternative_List/New_List.h
@@ -3,10 +3,11 @@ template<class T>class New_List_
 {
 protected:
 	New_List_ *prevPointer, *HEAD, *nextPointer, *TAIL;
-	T *data = nullptr;
+	T data = nullptr;
+	New_List_ *Pointer, *TEMPLATE;
 public:
 	New_List_();
-	void PUSH_BACK(T*);
+	void PUSH_BACK(T);
 	void POP_BACK(T);
 	void PUSH_FRONT(T);
 	void POP_FRONT(T);
--- Alternative_List/Alternative_List/New_Listcpp.cpp
+++ Alternative_List/Alternative_List/New_Listcpp.cpp
@@ -3,13 +3,28 @@
 
 CLASS1 New_List_<T>::New_List_()
 {
-	*HEAD = new New_List_;
+	*HEAD = nullptr
 	*TAIL = HEAD;
+	*Pointer = new New_List_;
 	*prevPointer = nullptr;
 	*nextPointer = nullptr;
+	*TEMPLATE = new New_List_;
 }
 
-template <class T> void New_List_<T>::PUSH_BACK(T *data)
+template <class T> void New_List_<T>::PUSH_BACK(T data)
 {
-	
+	Pointer->data = data;
+	Pointer->nextPointer = new New_List_;
+	if (Pointer->prevPointer == nullptr)
+		HEAD = Pointer;
+	Pointer->prevPointer = Pointer;
+	Pointer = Pointer->nextPointer;
+}
+
+CLASS1 void New_List_<T> ::POP_BACK(T data)
+{	
+	TEMPLATE->data = data;
+	TEMPLATE->nextPointer = HEAD;
+	TEMPLATE->prevPointer = nullptr;
+	HEAD = TEMPLATE;
 }
